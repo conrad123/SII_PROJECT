9a3ef73a1fa642228f6fd7e9702e0252aaf6ee1f
conrad123 <outerhaven@live.it>
2017-05-25 10:12:19 +0200

    abstracts module modified

--- abstracts.py
+++ abstracts.py
@@ -1,13 +1,13 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import json
 
-directories = ['BIO110']
+directories = ['PS001']
 
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 
 for directory in directories:
 
-    f = open('./outputfiles/dbpedia_spotlight/top_resources_biology_'+directory+'.txt')
+    f = open('./outputfiles/dbpedia_spotlight/top_resources_psychology_'+directory+'.txt')
     top_res = f.read()
     f.close()
 
@@ -19,26 +19,36 @@ for directory in directories:
 
         print article
 
+        final_result[article] = {}
+
         if top_res[article] != []:
-            res = top_res[article][0][0]
 
-        query = """
-                prefix dbpedia-owl: <http://dbpedia.org/ontology/>
-        
-                select ?abstract where {
-                    <"""+res+"""> dbpedia-owl:abstract ?abstract .
-                    filter(langMatches(lang(?abstract),"en"))
-                }
-                """
+            treshold = int((top_res[article][0][1]+top_res[article][len(top_res[article]-1)][1])/2)
 
-        sparql.setQuery(query)
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
+            res = [x for x in top_res[article] if x[1] >= treshold]
 
-        for result in results["results"]["bindings"]:
-            final_result[article] = result["abstract"]["value"]
+            for r in res:
 
-    f = open('./outputfiles/sparql/abstracts/abstracts_biology_'+directory+'.txt','w')
+                query = """
+                        prefix ontology: <http://dbpedia.org/ontology/>
+                
+                        select ?abstract where {
+                            <"""+r+"""> ontology:abstract ?abstract .
+                            filter(langMatches(lang(?abstract),"en"))
+                        }
+                        """
+
+                sparql.setQuery(query)
+                sparql.setReturnFormat(JSON)
+                results = sparql.query().convert()
+
+                for result in results["results"]["bindings"]:
+                    final_result[article][r] = result["abstract"]["value"]
+
+            else:
+                final_result[article][r] = ''
+
+    f = open('./outputfiles/sparql/abstracts/abstracts_psychology_'+directory+'.txt','w')
     f.write(json.dumps(final_result))
     f.close()
 
